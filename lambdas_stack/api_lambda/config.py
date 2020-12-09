3202ddf52651aaf746d9ef2bd38cc1f1d291770b
Rahul Lodha <rahullodha@Rahuls-MBP.fios-router.home>
2020-12-09 15:40:04 -0500

    send message endpoint and environment vars added

--- lambdas_stack/api_lambda/config.py
+++ lambdas_stack/api_lambda/config.py
@@ -5,6 +5,8 @@ from pydantic import BaseSettings
 
 class Config(BaseSettings):
     VERSION: str = None
+    SQS_ENDPOINT_URL: str = None
+    QUEUE_URL: str = None
 
 
 @lru_cache
--- lambdas_stack/api_lambda/endpoints.py
+++ lambdas_stack/api_lambda/endpoints.py
@@ -1,7 +1,12 @@
+import json
+from datetime import datetime
+
+import boto3
 from fastapi import APIRouter
 
 from common.logger import logger
 from config import get_config
+from common.models import MessageIn
 
 router = APIRouter()
 app_config = get_config()
@@ -14,4 +19,30 @@ def health_check():
     return {
         "api-status": "All Good!",
         "version": app_config.VERSION
+    }
+
+
+@router.post("/send")
+def send_message(message_in: MessageIn):
+    queue_payload = {
+        "message": message_in.message
+    }
+    sqs_client = boto3.client(
+        "sqs",
+        endpoint_url=app_config.SQS_ENDPOINT_URL
+    )
+    datetime_stamp = datetime.now().timestamp() * 1000
+    logger.info(f"sending message to queue: {app_config.QUEUE_URL}")
+    queue_response = sqs_client.send_message(
+        QueueUrl=app_config.QUEUE_URL,
+        MessageBody=json.dumps(queue_payload),
+        MessageGroupId="cf-test",
+        MessageDeduplicationId=f"cf-test-{datetime_stamp}"
+    )
+    logger.info(f"queue response: {json.dumps(queue_response)}")
+
+    return {
+        "response": {
+            "message_id": queue_response.get("MessageId")
+        }
     }
