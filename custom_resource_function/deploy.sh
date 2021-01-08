29922574010371bb4988ffbfc1dcef141ad5d96e
Rahul Lodha <rahul.lodha85@gmail.com>
2021-01-08 14:06:06 -0500

    Custom resource lambda (#7)
    * lambda versioning and alias with retain upon update
    
    * lambda versions and aliases
    
    * custom resource to publish lambda version and create lambda alias
    
    * updated custom resource lambda function for create, update and delete actions
    
    Co-authored-by: Rahul Lodha <rahullodha@Rahuls-MBP.fios-router.home>

---
+++ custom_resource_function/deploy.sh
@@ -0,0 +1,20 @@
+TEMPLATE=cf-deploy
+STACK_NAME=publish-alias-lambda
+S3_BUCKET=cf-templates-onmrt5w123ls-us-east-1
+
+pip install -r ./alias_publisher/requirements.txt -t ./external_dependencies/python/lib/python3.8/site-packages
+
+aws cloudformation validate-template --template-body file://${TEMPLATE}.yaml || exit 1
+
+aws cloudformation package \
+  --template-file ${TEMPLATE}.yaml \
+  --s3-bucket ${S3_BUCKET} \
+  --output-template-file ${TEMPLATE}_output.yaml || exit 1
+
+aws cloudformation deploy \
+  --template-file ${TEMPLATE}_output.yaml \
+  --stack-name ${STACK_NAME} \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --s3-bucket ${S3_BUCKET}
+
+aws cloudformation list-exports
